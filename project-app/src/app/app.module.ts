92f2a657d17d498fa1383f23404e6071c5d679d8
Ayoub <ayoub.elasry96@gmail.com>
2019-03-10 18:31:39 -0400

    config to read a json file

--- project-app/src/app/app.module.ts
+++ project-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { SquareMatrixPlotComponent } from "./visualizations/square-matrix-plots/square-matrix-plots.component";
+import { MapComponent } from './visualizations/map/map.component';
 
 @NgModule({
-  declarations: [AppComponent, SquareMatrixPlotComponent],
+  declarations: [AppComponent, SquareMatrixPlotComponent, MapComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ project-app/src/app/interfaces/JsonObject.ts
@@ -0,0 +1,13 @@
+export interface JsonObject {
+    date: string;
+    state: string;
+    city_or_county: string;
+    n_killed: number;
+    n_injured: number;
+    incident_characteristics: string;
+    latitude: number;
+    longitude: number;
+    participant_age: string;
+    participant_gender: string;
+    state_house_district: number;
+}
