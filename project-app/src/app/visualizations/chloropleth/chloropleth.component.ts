f0150b040491cb2fa1c0219662481fc5fc550272
Ayoub El-Asry <ayoub.elasry96@gmail.com>
2019-03-26 12:14:51 -0400

    toggle between states ans counties

--- project-app/src/app/visualizations/chloropleth/chloropleth.component.ts
+++ project-app/src/app/visualizations/chloropleth/chloropleth.component.ts
@@ -1,43 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import * as d3 from "d3";
 import * as topoJson from "topojson";
 
 @Component({
-  selector: 'app-chloropleth',
-  templateUrl: './chloropleth.component.html',
-  styleUrls: ['./chloropleth.component.css']
+  selector: "app-chloropleth",
+  templateUrl: "./chloropleth.component.html",
+  styleUrls: ["./chloropleth.component.css"]
 })
 export class ChloroplethComponent implements OnInit {
+  private type: string;
+  private svg: any;
+  private path: d3.GeoPath<any, d3.GeoPermissibleObjects>;
+  private stateData: any;
+  private countiesData: any;
+  private width = 960;
+  private height = 1160;
+  private us: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.type = "counties";
 
-    var width = 960,
-      height = 1160;
+    this.svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height);
 
-    var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+    this.path = d3.geoPath();
+    this.us = await d3.json("https://d3js.org/us-10m.v1.json");
+    this.svg.append("g").attr("class", "states");
 
-    var path = d3.geoPath();
-
-    d3.json("https://d3js.org/us-10m.v1.json").then((us: any) => {
-      console.log(us);
-
-      svg.append("g")
-        .attr("class", "states")
-        .selectAll("path")
-        .data(topoJson.feature(us, us.objects.states).features)
-        .enter().append("path")
-        .attr("d", path)
-        .style("fill", function (d) {
-          console.log(d)
-          return '#FF0000';
-        });
-
-    });
-
+    if (this.type === "counties") {
+      this.buildForCounties();
+    } else {
+      this.buildForStates();
+    }
   }
 
+  buildForCounties() {
+    this.svg.remove();
+    this.svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height);
+    this.svg
+      .selectAll("path")
+      .data(topoJson.feature(this.us, this.us.objects.counties).features)
+      .enter()
+      .append("path")
+      .attr("d", this.path);
+  }
+
+  buildForStates() {
+    this.svg.remove();
+    this.svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height);
+    this.svg
+      .selectAll("path")
+      .data(topoJson.feature(this.us, this.us.objects.states).features)
+      .enter()
+      .append("path")
+      .attr("d", this.path);
+  }
+
+  change() {
+    console.log("here");
+    if (this.type === "counties") {
+      this.type = "states";
+      this.buildForStates();
+    } else {
+      this.type = "counties";
+      this.buildForCounties();
+    }
+  }
 }
