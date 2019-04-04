a2d132d0c74c59d3d1db7bcd54eb28211ec1c390
marc el khoury <marc.el-khoury@polymtl.ca>
2019-04-04 18:31:03 -0400

    ajout du total

--- project-app/extract/Calcul-nombre-incident/map_data_sort_counties.py
+++ project-app/extract/Calcul-nombre-incident/map_data_sort_counties.py
@@ -12,6 +12,8 @@ with open(file_name, 'r') as csvFile:
 	data = json.load(csvFile)
 csvFile.close()
 
+county_years_dict["total"] = dict()
+
 for val in data:
     date = val["date"]
     if date[:4] in county_years_dict:
@@ -22,6 +24,11 @@ for val in data:
     else:
         county_years_dict[date[:4]] = dict()
         county_years_dict[date[:4]][val["city_or_county"]] = 1
+
+    if val["city_or_county"] in county_years_dict["total"]:
+        county_years_dict["total"][val["city_or_county"]] = county_years_dict["total"][val["city_or_county"]] + 1
+    else:
+        county_years_dict["total"][val["city_or_county"]] = 1
 
 with open('result_Counties.json', 'w') as fp:
     json.dump(county_years_dict, fp)
--- project-app/extract/Calcul-nombre-incident/map_data_sort_states.py
+++ project-app/extract/Calcul-nombre-incident/map_data_sort_states.py
@@ -12,6 +12,8 @@ with open(file_name, 'r') as csvFile:
 	data = json.load(csvFile)
 csvFile.close()
 
+county_years_dict["total"] = dict()
+
 for val in data:
     date = val["date"]
     if date[:4] in county_years_dict:
@@ -22,6 +24,13 @@ for val in data:
     else:
         county_years_dict[date[:4]] = dict()
         county_years_dict[date[:4]][val["state"]] = 1
+
+    if val["state"] in county_years_dict["total"]:
+        county_years_dict["total"][val["state"]] = county_years_dict["total"][val["state"]] + 1
+    else:
+        county_years_dict["total"][val["state"]] = 1
+
+
 
 with open('result_States.json', 'w') as fp:
     json.dump(county_years_dict, fp)
